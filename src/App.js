2c19d9f8502807b704ed4b84a5a0788328655010
Muskan1711 <muskanpage786@gmail.com>
2024-07-12 09:02:36 +0530

    changed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import XTable from './component/table';
+
+const App = () => {
+    return (
+        <div className="App">
+            <XTable />
+        </div>
+    );
+};
+
+export default App;
+
